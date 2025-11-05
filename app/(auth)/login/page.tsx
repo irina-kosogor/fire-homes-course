383022506cafc27a92c54e11acca6c6d3e733539
Iryna Kosogor <i.v.kosogor@gmail.com>
2025-11-05 11:48:17 +0200

    add basic routes and button

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,7 @@
+import ConinueWithGoogleButton from '@/components/continue-with-google-button';
+
+export default function Login() {
+    return <div>
+        <ConinueWithGoogleButton/>
+    </div>
+}
